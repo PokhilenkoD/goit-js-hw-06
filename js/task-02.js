27592bec5e5fd8297ebcacdf7e83d6467655d4e0
PokhilenkoD <pokhilenko.d@gmail.com>
2022-10-25 16:00:32 +0300

    adds solved second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const listEl = document.querySelector('#ingredients');
+const arrayElements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const itemEl = document.createElement('li');
+
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredients[i];
+  arrayElements.push(itemEl);
+}
+listEl.append(...arrayElements);
