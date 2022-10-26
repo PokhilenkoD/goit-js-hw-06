59bac96d76c3935907401859309b0942e6d24659
PokhilenkoD <pokhilenko.d@gmail.com>
2022-10-26 17:33:40 +0300

    adds solved eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const inputEmail = document.querySelector('[type="email"]');
+const inputPass = document.querySelector('[type="password"]');
+const btnLog = document.querySelector('[type="submit"]');
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSendForm);
+
+function onSendForm(event) {
+  event.preventDefault();
+
+  if (!inputEmail.value.length || !inputPass.value.length) {
+    alert('Пожалуйста, заполните все поля!');
+  } else {
+    console.log(`${inputEmail.name}:${inputEmail.value}\n${inputPass.name}:${inputPass.value}`);
+  }
+  formEl.reset();
+}
