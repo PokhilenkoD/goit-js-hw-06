72413278c11230be5ca10edce64a9fb4382303ed
PokhilenkoD <pokhilenko.d@gmail.com>
2022-10-27 18:05:40 +0300

    ads solved ninth task

--- js/task-08.js
+++ js/task-08.js
@@ -15,3 +15,4 @@ function onSendForm(event) {
   }
   formEl.reset();
 }
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+bodyEl.addEventListener('click', onChangeBackgroundColor);
+
+function onChangeBackgroundColor() {
+  const changeColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = changeColor;
+  spanEl.textContent = changeColor;
+}
